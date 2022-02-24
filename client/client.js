69da5dfb1e0f9e06e46a2c984791ddeffa35e7f5
dcs0spb <s.p.bradley@durham.ac.uk>
2022-02-24 02:07:42 +0000

    Comments copied into feedback area

--- client/client.js
+++ client/client.js
@@ -12,8 +12,9 @@ function compareEmail(email1, email2) {
   return comparison;
 }
 
-function title2ID(title) {
-  return title.replace(/ /g, '');
+function text2ID(text) {
+  // alphanumeric only
+  return text.replace(/\W/g, '');
 }
 
 async function init() {
@@ -24,44 +25,56 @@ async function init() {
   title.innerHTML = markscheme.assignment;
 
   const accordion = document.getElementById('feedback_options_accordion');
+  const feedback = document.getElementById('feedback');
   const sections = markscheme.sections;
   const schemes = markscheme.schemes;
 
   // from https://getbootstrap.com/docs/5.0/components/accordion/
 
   function sectionContent(section, parentSectionID) {
-    console.log("getting section content for ", section);
     let content = '';
-    const sectionID = title2ID(section.title);
+    const sectionID = text2ID(section.title);
     let schemeName = section.scheme;
     if (schemeName) {
       const scheme = schemes[schemeName];
-      content += `<div><select id="${sectionID}_${schemeName}>"`;
+      content += `<div><select name="${sectionID}_${schemeName}" id="${sectionID}_${schemeName}">`;
+      content += '<option></option>';
       for (let option of scheme.options) {
-        content += `<option>${option.description}</option>`;
+        let markText = option.mark ? `(${option.mark})` : '';
+        content += `<option value="${section.title}: ${option.description} ${markText}">${option.description}</option>`;
       }
       content += '</select></div>';
-      console.log("Looking at subsections", section.sections)
-
       if (section.sections) {
         for (let subsection of section.sections) {
-          content += `<div>${subsection.title}</div>`;
+          content += `<h4>${subsection.title}</h4>`;
           content += sectionContent(subsection, parentSectionID);
         }
       }
     }
     if (section.comments) {
       for (let comment of section.comments) {
-        content += `<div>${comment}</div>`;
+        // from https://getbootstrap.com/docs/5.0/forms/checks-radios/
+        let commentID = text2ID(comment)
+        content += `
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="* ${comment}" name="comment_${commentID}" id="check_${commentID}">
+          <label class="form-check-label" for="check_${commentID}">
+            ${comment}
+          </label>
+        </div>`;
       }
     }
     return content;
   }
 
+  let sectionIDs = [];
   for (let section of sections) {
-    let sectionID = title2ID(section.title);
+    let sectionID = text2ID(section.title);
+    sectionIDs.push(sectionID);
     let hID = `heading_${sectionID}`;
     let cID = `collapse_${sectionID}`;
+    let fID = `form_${sectionID}`;
+    let sID = `submit_${sectionID}`;
     let accordionItem = `
         <div class="accordion-item">
         <h2 class="accordion-header" id="${hID}">
@@ -71,13 +84,33 @@ async function init() {
         </h2>
         <div id="${cID}" class="accordion-collapse collapse" data-bs-parent="#feedback_options_accordion">
           <div class="accordion-body">
+          <form id="${fID}">
             ${sectionContent(section, sectionID)}
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
           </div>
         </div>
       </div>
         `;
     accordion.innerHTML += accordionItem;
+    feedback.innerHTML += `<h2>${section.title}</h2><div id="feedback_${sectionID}"></div>`;
   }
+
+  // need another loop because when innerHTML is updated the forms get recreated, and the event handlers lost
+  // could maybe use insertAdjacentHTML instead
+  for(let sectionID of sectionIDs){
+    let sectionForm = document.getElementById(`form_${sectionID}`);
+    let sectionFeedback = document.getElementById(`feedback_${sectionID}`);
+    sectionForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      let formContent = new FormData(sectionForm);
+      sectionFeedback.innerHTML = '';
+      for (let pair of formContent.entries()) {
+        sectionFeedback.innerHTML += `<div>${pair[1]}<div>`;
+      }
+    });
+  }
+
 
   const studentSelector = document.getElementById('student_selector');
   response = await fetch('comments.csv');
